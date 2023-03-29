2b4275deed43f9d1f684a46d84c6996eb32bf550
Johannes Blaschke <jpblaschke@lbl.gov>
2023-03-29 15:21:52 -0700

    update perlmutter install script to make use of new conda

--- cctbx/setup_perlmutter.sh
+++ cctbx/setup_perlmutter.sh
@@ -9,28 +9,28 @@ export KOKKOS_DEVICES="OpenMP;Cuda"
 export KOKKOS_ARCH="Ampere80"
 
 source ${ALCC_CCTBX_ROOT}/utilities.sh
-source ${ALCC_CCTBX_ROOT}/opt/util/fix_lib_nersc.sh
+# source ${ALCC_CCTBX_ROOT}/opt/util/fix_lib_nersc.sh
 source ${ALCC_CCTBX_ROOT}/opt/site/nersc_perlmutter.sh
 
 ${ALCC_CCTBX_ROOT}/update_bootstrap.sh
 
-fix-sysversions () {
-    env-activate
-    if fix-sysversions-perlmutter
-    then
-        return 1
-    fi
-}
+# fix-sysversions () {
+#     env-activate
+#     if fix-sysversions-perlmutter
+#     then
+#         return 1
+#     fi
+# }
 
 ${ALCC_CCTBX_ROOT}/opt/get_mamba_linux-64.sh
 
 load-sysenv
 
 mk-env cray-cuda-mpich-perlmutter perlmutter
-if fix-sysversions
-then
-    return 1
-fi
+# if fix-sysversions
+# then
+#     return 1
+# fi
 mk-cctbx cuda build hot update
 patch-dispatcher nersc
 
@@ -40,6 +40,8 @@ source ${ALCC_CCTBX_ROOT}/opt/site/nersc_perlmutter.sh
 load-sysenv
 activate
 
+export ALCC_CCTBX_ROOT=${ALCC_CCTBX_ROOT}
+export MODULES=${ALCC_CCTBX_ROOT}/modules
 export OMP_PLACES=threads
 export OMP_PROC_BIND=spread
 export KOKKOS_DEVICES="OpenMP;Cuda"
